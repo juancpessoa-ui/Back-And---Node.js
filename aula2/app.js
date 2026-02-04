de95c2d41b90b09c79af0ad8efdfda12a814cf34
JUAN CARLOS FONSECA PESSOA LOPES <25203642@CFP127ALUNO.LOCAL>
2026-02-04 10:28:16 -0300

    pasta back-and 04/01 aula 2 finalizado

--- aula2/app.js
+++ aula2/app.js
@@ -4,7 +4,16 @@
  * Autor: Juan Fonseca
  * Versão: 1.0.1.26
  ***********************************************************************************************************************************/
+                        /*Conversão de tipo de Dados:
 
+                        parseInt -> permite converter uma string opra numero INTEIRO
+                        parseFloat -> permite converter uma string para numero DECIMAL
+                        Number -> permite converte uma strig para numero NUMERO (INT OU FLOAT)
+                        String -> permite converter um conteúdo para STRING
+                        Boolean -> Permite converte um  conteúdO para BOOLEAN
+                        TYPEOF -> Permite verificar o tipo de dados de uma variavel
+
+                        */
 /* 
     Exitem 3 formas de criação de variavel
     
@@ -23,6 +32,24 @@
                 Recomendação: Caso voce queira diferenciar uma const, um var
                 ou let: A const pode ser criada com letra MAIUSCULA.
 */
+ /* 
+                        Operadores de Comparação: 
+                            == -> Permite comparar a igualdade de duas conteúdos.
+                            <  -> Permite comparar valores menores.
+                            >  -> Permite comparar valores maiores.
+                            >= -> Permite comparar valores maiores ou iguais. 
+                            <= -> Permite comparar valores menor ou iguais.
+                            != -> Permite comparar a diferença entre conteúdos.
+                            === -> Permite comparar a igualdade de conteúdo e a tipagem de dados.
+                            !== -> Permite comparar a diferença de conteúdos e a igualdadede tipos de dados.
+                            ==! -> Permite comparar a igualdade de conteúdos e a diferença de tipos de dados.
+                            !=! -> permite comparar a diferença de conteúdos e a diferença de de tipos de dados.
+
+                        Operadores Logicos:
+                            E   -> AND -> &&
+                            OU  -> OR  -> ||
+                            NÃO -> NOT  -> !
+                    */
 // import
 const readline = require('readline')
 //Cria Objeto para entrada
@@ -47,35 +74,34 @@ entradaDeDados.question('digite o nome do aluno: ', function(nome){
                 entradaDeDados.question('Digite a nota 4: ',function(valor4){
                     let nota4 = valor4
 
-                    /* 
-                        Operadores de Comparação: 
-                            == -> Permite comparar a igualdade de duas conteúdos.
-                            <  -> Permite comparar valores menores.
-                            >  -> Permite comparar valores maiores.
-                            >= -> Permite comparar valores maiores ou iguais. 
-                            <= -> Permite comparar valores menor ou iguais.
-                            != -> Permite comparar a diferença entre conteúdos.
-                            === -> Permite comparar a igualdade de conteúdo e a tipagem de dados.
-                            !== -> Permite comparar a diferença de conteúdos e a igualdadede tipos de dados.
-                            ==! -> Permite comparar a igualdade de conteúdos e a diferença de tipos de dados.
-                            !=! -> permite comparar a diferença de conteúdos e a diferença de de tipos de dados.
-
-                        Operadores Logicos:
-                            E   -> AND -> &&
-                            OU  -> OR  -> ||
-                            NÃO -> NOT  -> !
-                    */
+                   
                     //Validação de entrada vazia
                     if(nomeAluno == '' || nota1  == '' || nota2 == '' || nota3 == '' || nota4 == ''){
                         console.log('Erro: é obrigatorio o preenchimento de todos os dados!!!')
-                        
+                    //Validação de entrada de 0 a 100   
                     }else if( nota1 <0 || nota1 >100 || nota2 <0 || nota2 >100 || nota3 <0 || nota3 >100 || nota4 <0 || nota4 >100  ){
                         console.log('Erro: é obrigatorio o valor de notas valida!!!')
+                    //Validação de entrada de letras
+                    // isNan() -> Permite validar se o conteúdo da variavel tem algum caracter ao inves de número.
+                    }else if(isNaN(nota1) || isNaN(nota2)  || isNaN(nota3)  || isNaN(nota4)){
+                        console.log(' ERRO: Não é possivel calcular a media com a esntrada e letra nas notas do aluno!!! ')
+                        
+                    //Calculo e Validação de media  
                     }else{
-                        // calcular media+
+                        let statusAluno
+                        let media = (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4))/ 4
+                        
+                        //Validação do Status de aprovação do aluno
+                        if(media >= 70){
+                            statusAluno = 'Aprovado'
+                        }else if(media >= 50 && media < 70 ){
+                            statusAluno = 'Recuperação'
+                        }else{
+                            statusAluno = 'Reprovado'  
+                        }
+                        // Saída da Média
+                        console.log(`O aluno(a) ${nomeAluno} ficou com média ${media.toFixed(2)} e esta: ${statusAluno}`)
                     }
-
-
                 })
             })
         })
